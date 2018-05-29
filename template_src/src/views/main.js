f0e782fb209064949ff697d5139fd976b6fbba1f
Yerko Palma <yerko.palma@usach.cl>
2018-05-29 21:57:31 +0000

    improve dev start

--- template_src/src/views/main.js
+++ template_src/src/views/main.js
@@ -1,4 +1,11 @@
 var html = require('choo/html')
+var css = require('sheetify')
+
+var animate = css`
+  :host {
+    transition: all 0.3s
+  }
+`
 
 var TITLE = 'src - main'
 
@@ -10,7 +17,7 @@ function view (state, emit) {
   return html`
     <body class="code lh-copy w-100 vh-100 dt">
       <main class="pa3 cf bg-washed-red vh-100 dtc v-mid tc">
-        <div id="touche" class="bg-washed-blue shadow-1 center pa3 br3 w5 h5"></div>
+        <div id="touche" class="${animate} bg-washed-blue shadow-1 center pa3 br3 w5 h5"></div>
       </main>
     </body>
   `
