232534fa38125191e095379418d0e6312de9fb2e
Yerko Palma <yerko.palma@usach.cl>
2018-05-31 21:20:11 +0000

    tweaks

--- template_src/src/views/main.js
+++ template_src/src/views/main.js
@@ -7,13 +7,9 @@ var animate = css`
   }
 `
 
-var TITLE = 'src - main'
-
 module.exports = view
 
 function view (state, emit) {
-  if (state.title !== TITLE) emit(state.events.DOMTITLECHANGE, TITLE)
-
   return html`
     <body class="code lh-copy w-100 vh-100">
       <main class="pa3 cf bg-washed-red vh-100">
@@ -22,7 +18,7 @@ function view (state, emit) {
           data-recognize="pan" 
           data-panstart="drag" 
           data-panmove="drag" 
-          class="${animate} bg-washed-blue shadow-1 center pa3 br3 w5 h5 absolute"></div>
+          class="${animate} bg-washed-blue shadow-1 center pa3 br3 w4 h4 absolute"></div>
       </main>
     </body>
   `
